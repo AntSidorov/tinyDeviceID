16f4be79f0efa5ae513aea1f91806c41a3542bf7
AntSidorov <53177941+AntSidorov@users.noreply.github.com>
2019-11-05 10:55:18 +0500

    1

--- app/src/main/java/com/android/tinydeviceid/MainActivity.java
+++ app/src/main/java/com/android/tinydeviceid/MainActivity.java
@@ -2,12 +2,15 @@ package com.android.tinydeviceid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity{
 
     TextView DeviceID;
+    Button btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,13 @@ public class MainActivity extends AppCompatActivity{
         String ANDROID_ID = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
         DeviceID = (TextView) findViewById(R.id.DeviceID);
         DeviceID.setText(ANDROID_ID);
+
+        btnClose = (Button) findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
